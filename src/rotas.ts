736c11c0043291190813d84f9de547f7142f9c41
Nanica.log <farlany.domingues@estudante.ifms.edu.br>
2025-09-23 12:48:09 -0300

    farlany 2 enviou

--- src/rotas.ts
+++ src/rotas.ts
@@ -1,21 +1,14 @@
-import usuarioController from "./usuarios/usuario.controller.js";
-import produtoController from "./produtos/produto.controller.js";
-
+import usuarioController from "./usuarios/usuario.controller";
+import produtoController from "./produtos/produto.controller"; 
 import { Router } from "express";
 
 const rotas = Router();
 
-//Criando rotas para os usuários
 rotas.post("/usuarios", usuarioController.adicionar);
 rotas.get("/usuarios", usuarioController.listar);
 
 
-//rotas para produtos
-rotas.post("/produtos", produtoController.adicionar);
-rotas.get("/produtos", produtoController.listar);
-
-
-//Ainda vamos ter que criar as rotas para carrinho e produtos
-//Tarefa para casa :)
+rotas.post("/produtos", produtoController.adicionar); 
+rotas.get("/produtos", produtoController.listar);   
 
 export default rotas;
