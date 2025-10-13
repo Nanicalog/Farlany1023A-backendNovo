8a0c2a1cc9661686e04c020a788b12705add8247
Nanica.log <farlany.domingues@estudante.ifms.edu.br>
2025-10-13 11:03:39 -0300

    #isntalçao do bcrypt

--- src/carrinho/carrinho.controller.ts
+++ src/carrinho/carrinho.controller.ts
@@ -1,101 +1,173 @@
 import { Request, Response } from "express";
+import { ObjectId } from "bson";
 import { db } from "../database/banco-mongo.js";
-import { ObjectId } from "mongodb";
-
+import { ListSearchIndexesCursor } from "mongodb";
 
 interface ItemCarrinho {
     produtoId: string;
-    nome: string;
-    preco: number;
     quantidade: number;
+    precoUnitario: number;
+    nome: string;
 }
 
 interface Carrinho {
-    usuarioId: ObjectId;
+    usuarioId: string;
     itens: ItemCarrinho[];
+    dataAtualizacao: Date;
     total: number;
-    modificacao: Date;
 }
+
+interface Produto {
+    _id: ObjectId;
+    nome: string,
+    preco: number,
+    descricao: string,
+    urlfoto: string
+}
+
 class CarrinhoController {
-    // GET 
-    async getCarrinho(req: Request, res: Response) {
-        const { usuarioId } = req.params;
-        if (!usuarioId) {
-            return res.status(400).json({ message: "usuarioId é obrigatório" });
-        }
+    //adicionarItem
+    async adicionarItem(req: Request, res: Response) {
+        const { usuarioId, produtoId, quantidade } = req.body as { usuarioId: string, produtoId: string, quantidade: number };
+        console.log(usuarioId, produtoId, quantidade)
 
-        const carrinho = await db.collection<Carrinho>("carrinhos").findOne({ usuarioId: ObjectId.createFromHexString(usuarioId) });
+        //Buscar o produto no banco de dados
+        const produto = await db.collection<Produto>('produtos')
+            .findOne({ _id: ObjectId.createFromHexString(produtoId) });
+        if (!produto)
+            return res.status(404).json({ mensagem: 'Produto não encontrado' });
+        //Pegar o preço do produto
+        //Pegar o nome do produto
+        const nomeProduto = produto.nome;
+        const precoProduto = produto.preco;
 
-        if (!carrinho) {
-            return res.status(404).json({ message: "Carrinho não encontrado" });
-        }
-
-        res.json(carrinho);
-    }
-
-    // POST 
-    async adicionar(req: Request, res: Response) {
-        const { usuarioId, produtoId, nome, preco, quantidade } = req.body;
-
-        if (!usuarioId || !produtoId || !nome || !preco || quantidade == null) {
-            return res.status(400).json({ message: "Campos obrigatórios: usuarioId, produtoId, nome, preco, quantidade" });
-        }
-
-        if (quantidade <= 0) {
-            return res.status(400).json({ message: "Quantidade inválida" });
-        }
-
-        let carrinho = await db.collection<Carrinho>("carrinhos").findOne({ usuarioId: ObjectId.createFromHexString(usuarioId) });
+        // Verificar se um carrinho com o usuário já existe
+        const carrinho = await db.collection<Carrinho>("carrinhos").findOne({ usuarioId: usuarioId });
 
         if (!carrinho) {
             const novoCarrinho: Carrinho = {
-                usuarioId: ObjectId.createFromHexString(usuarioId),
-                itens: [{ produtoId, nome, preco, quantidade }],
-                total: preco * quantidade,
-                modificacao: new Date(),
-            };
+                usuarioId: usuarioId,
+                itens: [{
+                    produtoId: produtoId,
+                    quantidade: quantidade,
+                    precoUnitario: precoProduto,
+                    nome: nomeProduto
+                }],
+                dataAtualizacao: new Date(),
+                total: precoProduto * quantidade
+            }
+            const resposta = await db.collection<Carrinho>("carrinhos").insertOne(novoCarrinho);
+            const carrinhoResposta = {
+                usuarioId: novoCarrinho.usuarioId,
+                itens: novoCarrinho.itens,
+                dataAtualizacao: novoCarrinho.dataAtualizacao,
+                total: novoCarrinho.total,
+                _id: resposta.insertedId
 
-            await db.collection("carrinhos").insertOne(novoCarrinho);
-            return res.status(201).json(novoCarrinho);
+            }
+            //return res.status(201).json({...novoCarrinho, _id: resposta.insertedId});
+
+            //Early Return
+            return res.status(201).json(carrinhoResposta);
+
         }
-
-        const itemExistente: ItemCarrinho | undefined = carrinho.itens.find((i: ItemCarrinho) => i.produtoId === produtoId);
-
+        //ELSE
+        // Se existir, deve adicionar o item ao carrinho existente
+        const itemExistente = carrinho.itens.find(item => item.produtoId === produtoId);
         if (itemExistente) {
             itemExistente.quantidade += quantidade;
-        } else {
-            carrinho.itens.push({ produtoId, nome, preco, quantidade });
+            carrinho.total += precoProduto * quantidade;
+            carrinho.dataAtualizacao = new Date();
         }
-
-        carrinho.total = carrinho.itens.reduce(
-            (acc: number, item: ItemCarrinho): number => acc + item.preco * item.quantidade,
-            0
-        );
-
-        carrinho.modificacao = new Date();
-
-        await db.collection("carrinhos").updateOne(
-            { usuarioId: ObjectId.createFromHexString(usuarioId) },
+        else {
+            carrinho.itens.push({
+                produtoId: produtoId,
+                quantidade: quantidade,
+                precoUnitario: precoProduto,
+                nome: nomeProduto
+            });
+            carrinho.total += precoProduto * quantidade;
+            carrinho.dataAtualizacao = new Date();
+        }
+        // Atualizar o carrinho no banco de dados
+        await db.collection<Carrinho>("carrinhos").updateOne({ usuarioId: usuarioId },
             {
-                $set:
-                {
+                $set: {
                     itens: carrinho.itens,
                     total: carrinho.total,
-                    modificacao: carrinho.modificacao
+                    dataAtualizacao: carrinho.dataAtualizacao
                 }
             }
-        );
+        )
+        res.status(200).json(carrinho);
+    }
+    //remove item
+    removerItem(req: Request, res: Response) {
+        const { produtoIdd, usuarioId } = req.body;
+
+        if (!usuarioId) {
+            return res.status(400).json({ mensagem: "usuarioId é obrigatório" });
+        }
+        if (!produtoIdd) {
+            return res.status(400).json({ mensagem: "produtoId é obrigatório" });
+        }
+        
+    }
+    //atualizarQuantidade
+    async atualizaQuantidade(req: Request, res: Response) {
+        const { produtoId, usuarioId, quantidade } = req.body;
+        
+        const carrinho = await db.collection<Carrinho>("carrinhos").findOne({ usuarioId });
+        if (!carrinho)
+            return res.status(404).json({ mensagem: "Carrinho não encontrado" });
+
+        const item = carrinho.itens.find(i => i.produtoId === produtoId);
+        if (!item)
+            return res.status(404).json({ mensagem: "Produto não encontrado no carrinho" });
+
+        item.quantidade = quantidade;
+
+        carrinho.total = carrinho.itens.reduce((acc, i) => acc + i.quantidade * i.precoUnitario, 0);
+        carrinho.dataAtualizacao = new Date();
+
+        await db.collection<Carrinho>("carrinhos").updateOne({ usuarioId }, { $set: carrinho });
+        return res.status(200).json(carrinho);
+    }
+        //listar quantidade
+    async listar(req: Request, res: Response) {
+        const { usuarioId } = req.body;
+        if (!usuarioId) {
+            return res.status(400).json({ mensagem: "usuarioId é obrigatório" });
+        }
+
+        const carrinho = await db.collection<Carrinho>("carrinhos").findOne({ usuarioId });
+        if (!carrinho) {
+            return res.status(404).json({ mensagem: "Carrinho não encontrado" });
+        }
 
         return res.status(200).json(carrinho);
+    }
+//remover carrinho
+    async remover(req: Request, res: Response) {
+        const { usuarioId } = req.body;
+        if (!usuarioId) {
+            return res.status(400).json({ mensagem: "usuarioId é obrigatório" });
+        }
+
+        const resultado = await db.collection<Carrinho>("carrinhos").deleteOne({ usuarioId });
+        if (resultado.deletedCount === 0)
+            return res.status(404).json({ mensagem: "Carrinho não encontrado" });
+
+        return res.status(200).json({ mensagem: "Carrinho removido com sucesso" });
     }
 }
 
 export default new CarrinhoController();
 
-//buscar o preço do produto
-//pegar o nome do produto
 
-//verificar se um carrinho com o usuario ja existe
-//se existir, criar um novo carrinho
-//se não existir, deve criar um novo carrinho
-//  existente
+//atualizarQuantidade
+
+//listar
+//remover   -> Remover o carrinho todo
+
+
--- src/usuarios/usuario.controller.ts
+++ src/usuarios/usuario.controller.ts
@@ -1,15 +1,32 @@
 import { Request, Response } from "express";
 import { db } from "../database/banco-mongo.js";
+import bcrypt from 'bcrypt'
+
+
 class UsuarioController {
     async adicionar(req: Request, res: Response) {
-        const estudante = req.body
-        const resultado = await db.collection('estudantes')
-            .insertOne(estudante)
-        res.status(201).json({ ...estudante, _id: resultado.insertedId })
+        const {nome,idade,email,senha} = req.body
+    
+        if(!nome || !idade || !email || !senha){ //validação
+             return res.status(400).json({mensagem: "Ddaos incpompletos (nome,email,senha,idade)"})
+        }
+        const usuario = { nome, idade, email, senha}
+        const resultado = await db.collection('usuarios')
+            .insertOne(usuario)
+        res.status(201).json({ ...usuario, _id: resultado.insertedId })
     }
     async listar(req: Request, res: Response) {
-        const estudantes = await db.collection('estudantes').find().toArray();
-        res.status(200).json(estudantes);
+        const usuarios = await db.collection('usuarios').find().toArray();
+        res.status(200).json(usuarios);
     }
 }
+
+async function login(req: Request, res: Response) {
+    
+}
 export default new UsuarioController();
+
+
+
+
+
